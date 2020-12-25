35678a3ea50194e04750c9d617d26a974b08fb31
Zee98 <zamanehsani@gmail.com>
2020-12-25 23:01:11 +0400

    initial commit

---
+++ website/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+
+def index(request):
+    return render(request, "index.html")
+
+def blog(request):
+    return render(request, "blog.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def initiatives(request):
+    return render(request, "initiatives.html")
+
+def report(request):
+    return render(request, "report.html")
+
+def team(request):
+    return render(request, "team.html")
